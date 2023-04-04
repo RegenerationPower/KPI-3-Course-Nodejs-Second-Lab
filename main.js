1e61dbb38b431c5ee014d341ee1da2bf619baaac
Voznytsia <dmitryvoznitsa@gmail.com>
2023-04-04 22:42:35 +0300

    Lab2 done

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ const http = require("http");
 const url = 'https://www.bbc.com/ukrainian';
 const dir = './news';
 
+const server = http.createServer();
+const PORT = 8082;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
 if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
 } else {
@@ -16,6 +20,7 @@ if (!fs.existsSync(dir)) {
 
 function scrapeData() {
     let counter = 1;
+
     https.get(url, (response) => {
         let data = '';
 
@@ -25,51 +30,68 @@ function scrapeData() {
 
         response.on('end', () => {
             const $ = cheerio.load(data);
-            $('p:not(.bbc-1e1hq0), span').each((i, elem) => {
+            $('p:not(.bbc-1e1hq0), main').each((i, elem) => {
                 const text = $(elem).text();
-                const excluded = ['BBC News', '©', 'Подкасти', 'Фотогалереї', 'Поточна сторінка,', 'Блог психолога',
-                    'Подивитись все', 'Здоров\'я', 'Top story - Ukrainian', 'Наука і технології', ', Тривалість ',
-                    'Журнал ВВС', 'Суспільство', 'Вибір редакції', 'Відео', 'Розділи', 'Війна з Росією', 'Історії',
-                    'Книга року BBC', 'Економіка', 'Читайте також', 'Огляд преси', 'Політика', 'Наш YouTube', 'Докладно'];
-                if (!excluded.some((s) => text.includes(s))) {
-                    console.log(text);
-                    const filename = `${dir}/${counter}.txt`;
-                    fs.writeFile(filename, text, (err) => {
+                // Ігноруємо перший файл, у якому міститься уся інфа з мейну
+                if (counter > 1) {
+                    const filename = `${dir}/${counter - 1}.txt`;
+                    fs.writeFile(filename, text, { encoding: 'utf-8' }, (err) => {
                         if (err) throw err;
                         console.log(`File ${filename} saved`);
                     });
-                    counter++;
                 }
+                counter++;
             });
 
-            const server = http.createServer();
-            const PORT = 8081;
-
+            console.log("");
             server.on('connection', handleConnection);
-            server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
 
             function handleConnection(socket) {
-                const files = fs.readdirSync(dir).sort((a, b) => {
-                    const numA = parseInt(a);
-                    const numB = parseInt(b);
-                    if (numA < numB) return -1;
-                    if (numA > numB) return 1;
-                    return 0;
-                });
-                // const files = fs.readdirSync(dir);
-                const response = files.join('\n');
-                if (socket.writable) {
-                    socket.write(`HTTP/1.1 200 OK\r\nContent-Length: ${response.length}\r\n\r\n${response}`);
-                    socket.end();
-                }
+                socket.on('data', (data) => {
+                    const url = data.toString().split(' ')[1];
+                    if (url === '/') {
+                        const files = fs.readdirSync(dir).sort((a, b) => {
+                            const A = parseInt(a);
+                            const B = parseInt(b);
+                            if (A < B) return -1;
+                            if (A > B) return 1;
+                            return 0;
+                        });
 
-                socket.on('error', (error) => {
-                    console.error(`Socket error happened: ${error.message}`);
+                        let html = '<!DOCTYPE html><html lang="en-GB"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>Lab2</title></head><body><ul>';
+
+                        files.forEach(file => {
+                            const id = parseInt(file.split('.')[0]);
+                            html += `<li><a href="/${id}">Новина ${id}</a></li>`;
+                        });
+
+                        html += '</ul>';
+                        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n${html}`);
+                        socket.end();
+
+                    } else {
+                        const fileNumber = parseInt(url.split('/')[1]);
+                        const filename = `${dir}/${fileNumber}.txt`;
+
+                        fs.readFile(filename, (err, content) => {
+                            if (err) {
+                                console.error(err);
+                                socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+                                socket.end();
+                            } else {
+                                socket.write(`HTTP/1.1 200 OK\r\nContent-Length: ${content.length}\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n${content}`);
+                                socket.end();
+                            }
+                        });
+                    }
                 });
             }
+            setTimeout(scrapeData, 60000);
         });
-    })
+    }).on('error', (err) => {
+        console.error(`Error while scraping data: ${err.message}`);
+        setTimeout(scrapeData, 60000);
+    });
 }
 
 scrapeData();
-setInterval(scrapeData, 60000);
